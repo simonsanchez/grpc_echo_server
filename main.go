a6468faff6a4e65f9af4b15e3e19950bbb1574e7
Simon Sanchez <ssanchez@apartmentlist.com>
2021-07-09 15:11:04 -0700

    Update version of `grpc_echo_proto_two`
    This one implements the `Foo` rpc.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	pb "github.com/simonsanchez/grpc_echo_proto_two/generated"
 	"google.golang.org/grpc"
@@ -27,5 +28,10 @@ type server struct {
 
 func (s *server) Echo(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
 	fmt.Printf("message recieved by server: %s\n", in.GetMessage())
+	return &pb.EchoResponse{Message: in.GetMessage()}, nil
+}
+
+func (s *server) Foo(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+	fmt.Println("foo -- " + time.Now().String())
 	return &pb.EchoResponse{Message: in.GetMessage()}, nil
 }
